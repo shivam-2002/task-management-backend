2a69dfe2a245f84ab040455b2546676699372dec
Shivam Kuar <rahul.shivam64@gmail.com>
2025-03-01 18:55:59 +0530

    fixed error

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,13 @@ import taskRoutes from "./routes/taskRoutes";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_ORIGIN }));
+app.use(cors({
+    origin: process.env.FRONTEND_ORIGIN,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}));
 app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
